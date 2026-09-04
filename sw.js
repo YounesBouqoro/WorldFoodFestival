4b5a470474cb70880f6f4ba9bdf8bbc55d8ded6c
YounesBouqoro <younesbouqoro@gmail.com>
2026-09-04 20:48:29 +0200

    Refresh iPhone install helper assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='wff-pos-shell-v17';
+const CACHE_NAME='wff-pos-shell-v18';
 const LOCAL_ASSETS=[
   './','./index.html','./drinks.html','./admin.html','./receipt.html',
   './styles.css','./access.css','./deposit.css','./mobile-pos.css','./receipt-actions.css','./admin.css','./receipt.css','./pwa.css','./mobile-ui.css',
